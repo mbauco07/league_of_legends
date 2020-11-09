10cb2347016d3ec44ef86f8fe484a3c055361b3f
Marco Bauco <31773670+mbauco07@users.noreply.github.com>
2020-11-08 21:16:39 -0500

    Completed both a basic stats output and per level stats output

--- champion_stats.py
+++ champion_stats.py
@@ -1,49 +1,185 @@
 #This script will ask for a champ or champs and will get their stats, stats per level, etc and then compare build with items included.
-import keys
+import keys as ks
+import special_scaling_values as ssv
 import requests
 import sys
+import math
 from prettytable import PrettyTable
 
 #------------------------------------------------------------ FUNCTIONS ------------------------------------------------------------#
-def output_basic_stats(_data, _championName):   
-    x = PrettyTable()
-    x.field_names=["Stat", "Level 1", "Level 18", "per level"]
-    x.add_row(['Health', _data['hp'], (_data['hp']+(_data['hpperlevel']*17)), _data['hpperlevel']])
-    x.add_row(['Mana/Resource', _data['mp'], (_data['mp']+(_data['mpperlevel']*17)), _data['mpperlevel']])
-    x.add_row(['Move Speed', _data['movespeed'], _data['movespeed'], 'N/A'])
-    x.add_row(['Armor', _data['armor'], (_data['armor']+(_data['armorperlevel']*17)), _data['armorperlevel']])
-    x.add_row(['Magic Resistance', _data['spellblock'], (_data['spellblock']+(_data['spellblockperlevel']*17)), _data['spellblockperlevel']])
-    x.add_row(['Attack Range', _data['attackrange'], _data['attackrange'], 'N/A'])
-    x.add_row(['HP Regeneration', _data['hpregen'], (_data['hpregen']+(_data['hpregenperlevel']*17)), _data['hpregenperlevel']])
-    x.add_row(['Resource Regeneration', _data['mpregen'], (_data['mpregen']+(_data['mpregenperlevel']*17)), _data['mpregenperlevel']])
-    x.add_row(['Critical Strike', _data['crit'], (_data['crit']+(_data['critperlevel']*17)), _data['critperlevel']])
-    x.add_row(['Attack Damage', _data['attackdamage'], (_data['attackdamage']+(_data['attackdamageperlevel']*17)), _data['attackdamageperlevel']])
-    x.add_row(['Attack Speed', _data['attackspeed'], (_data['attackspeed']+(_data['attackspeedperlevel']*17)), _data['attackspeedperlevel']])
-    print(x)
+#we need to account for champions that have a special scaling that is different then the rest of the champions (I.E. Tristanna or Cassieopia) because of this
+#we must check to see if we are dealing with one of these special cases and adjust our output for it.
+#this will also only output levels 1 and 18 not in between
+def Basic_Stats_Output(_data, _championName):   
+    basic_table = PrettyTable()
+    basic_table.title = 'Stats for: ' + _championName + ' at Level 1 and 18'
+
+    basic_table.field_names=["Stat", "Level 1", "Level 18", "per level"]
+    basic_table.add_row(['Health', _data['hp'], (_data['hp']+(_data['hpperlevel']*17)), _data['hpperlevel']])
+    basic_table.add_row(['Mana/Resource', _data['mp'], (_data['mp']+(_data['mpperlevel']*17)), _data['mpperlevel']])
+
+    #are we dealing with Cassiopeia?
+    if _championName != 'Cassiopeia':
+        basic_table.add_row(['Move Speed', _data['movespeed'], _data['movespeed'], 'N/A'])
+    else: #we are dealing with Cassiopeia
+        basic_table.add_row(['Move Speed***', _data['movespeed']+ssv.cass_ms_per_level , (_data['movespeed']+ssv.cass_ms_per_level)+(ssv.cass_ms_per_level*17), ssv.cass_ms_per_level])
+
+    basic_table.add_row(['Armor', normal_round(_data['armor']), (normal_round(_data['armor']+(_data['armorperlevel']*17))), _data['armorperlevel']])
+    basic_table.add_row(['Magic Resistance', round(_data['spellblock']), normal_round((_data['spellblock']+(_data['spellblockperlevel']*17))), _data['spellblockperlevel']])
+
+    #are we dealing with Tristana?
+    if _championName != 'Tristana':
+        basic_table.add_row(['Attack Range', _data['attackrange'], _data['attackrange'], 'N/A'])
+    else:
+        basic_table.add_row(['Attack Range***', _data['attackrange'], _data['attackrange']+(ssv.tristanna_range_per_level*17), ssv.tristanna_range_per_level])
+    
+    basic_table.add_row(['HP Regeneration', round(_data['hpregen']), normal_round((_data['hpregen']+(_data['hpregenperlevel']*17))), _data['hpregenperlevel']])
+    basic_table.add_row(['Mana/Resource Regeneration', round(_data['mpregen']), normal_round((_data['mpregen']+(_data['mpregenperlevel']*17))), _data['mpregenperlevel']])
+    basic_table.add_row(['Critical Strike', _data['crit'], (_data['crit']+(_data['critperlevel']*17)), _data['critperlevel']])
+    basic_table.add_row(['Attack Damage', round(_data['attackdamage']), round((_data['attackdamage']+(_data['attackdamageperlevel']*17))), _data['attackdamageperlevel']])
+    basic_table.add_row(['Attack Speed', round(_data['attackspeed'],2), round(((round(_data['attackspeed'],2)*((_data['attackspeedperlevel']*17)))/100) + round(_data['attackspeed'],2),2) , _data['attackspeedperlevel']])
+    print(basic_table)
+
+#this will work similliarly to Basic_Stats_Output however instead this will output each level for a champion
+def Per_Level_Stats_output(_data, _championName):
+    pls_table = PrettyTable()
+    pls_table.title = 'Stats for: ' + _championName + ' at each level'
+    Per_Level_Table_Helper_no_Dec(pls_table, 'Level' , 1, 1)
+    Per_Level_Table_Helper_no_Dec(pls_table, 'Health', _data['hp'], _data['hpperlevel'])
+    Per_Level_Table_Helper_no_Dec(pls_table, 'Mana/Resource', _data['mp'], _data['mpperlevel'])
+
+    #are we dealing with Cassiopeia?
+    if _championName != 'Cassiopeia':
+        Per_Level_Table_Helper_no_Dec(pls_table, 'Move Speed', _data['movespeed'], 0)
+    else: #we are dealing with Cassiopeia
+        Per_Level_Table_Helper_no_Dec(pls_table, 'Move Speed', _data['movespeed']+ssv.cass_ms_per_level, ssv.cass_ms_per_level)
+
+    Per_Level_Table_Helper_no_Dec(pls_table, 'Armor', _data['armor'], _data['armorperlevel'])
+    Per_Level_Table_Helper_no_Dec(pls_table, 'Magic Resistance', _data['spellblock'], _data['spellblockperlevel'])
+
+    #are we dealing with Tristana?
+    if _championName != 'Tristana':
+        Per_Level_Table_Helper_no_Dec(pls_table, 'Attack Range', _data['attackrange'], 0)
+    else:
+        Per_Level_Table_Helper_no_Dec(pls_table, 'Attack Range', _data['attackrange'], ssv.tristanna_range_per_level)
+
+    Per_Level_Table_Helper_no_Dec(pls_table, 'HP Regeneration', _data['hpregen'], _data['hpregenperlevel'])
+    Per_Level_Table_Helper_no_Dec(pls_table, 'Critical Strike', _data['crit'], _data['critperlevel'])
+    Per_Level_Table_Helper_no_Dec(pls_table, 'Attack Damage', _data['attackdamage'], _data['attackdamageperlevel'])
+    Per_Level_Table_Helper_2_Dec_Attack_Speed(pls_table, 'Attack Speed', round(_data['attackspeed'],2), _data['attackspeedperlevel'])
+
+   
+    print(pls_table)
+ 
+def Per_Level_Table_Helper_no_Dec(_table,_statName, _startingValue, _perLevelValue):
+    _table.add_column(
+        _statName, [ 
+       normal_round(_startingValue+(_perLevelValue*0)),   
+        normal_round(_startingValue+(_perLevelValue*1)),  
+        normal_round(_startingValue+(_perLevelValue*2)),  
+        normal_round(_startingValue+(_perLevelValue*3)),   
+        normal_round(_startingValue+(_perLevelValue*4)),  
+        normal_round(_startingValue+(_perLevelValue*5)),   
+        normal_round(_startingValue+(_perLevelValue*6)),   
+        normal_round(_startingValue+(_perLevelValue*7)),   
+        normal_round(_startingValue+(_perLevelValue*8)),  
+        normal_round(_startingValue+(_perLevelValue*9)),  
+        normal_round(_startingValue+(_perLevelValue*10)),   
+        normal_round(_startingValue+(_perLevelValue*11)),   
+        normal_round(_startingValue+(_perLevelValue*12)),   
+        normal_round(_startingValue+(_perLevelValue*13)),   
+        normal_round(_startingValue+(_perLevelValue*14)),   
+        normal_round(_startingValue+(_perLevelValue*15)),   
+        normal_round(_startingValue+(_perLevelValue*16)),   
+        normal_round(_startingValue+(_perLevelValue*17)),   
+    ])    
+
+def Per_Level_Table_Helper_2_Dec_Attack_Speed(_table,_statName, _startingValue, _perLevelValue):
+    _table.add_column(
+        _statName, [ 
+        round(_startingValue+(_startingValue*(_perLevelValue*0)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*1)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*2)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*3)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*4)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*5)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*6)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*7)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*8)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*9)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*10)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*11)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*12)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*13)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*14)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*15)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*16)/100),2),   
+        round(_startingValue+(_startingValue*(_perLevelValue*17)/100),2)          
+    ])  
+
+def Item_CleanUp(items):
+    #print(items[1])
+    for item in items: 
+        print(item['name'])
+        print(item['description'])
+        print(item['gold']['total'])
+        print(item['stats'])
+        print('------------------------------')
+#for some champion names that have a space or a special characther, they need to be removed so that champion can be correctly found
+def validate_champInput(_champion):
+    #check if the champion name has a space in it.
+    if  ' ' in _champion:
+        _champion  =_champion.replace(' ', '')
+    
+    if '\'' in _champion:
+        x  =  _champion.split("\'", 2)
+        _champion  =  x[0]+''+x[1].lower()
+
+    return _champion
+
+#we use this for values we round to the ones because of how python takes care of rounding on x.5.
+#using round() is will always round down in these situations however we need to to round up instead
+def normal_round(n):
+    if n - math.floor(n) < 0.5:
+        return math.floor(n)
+    return math.ceil(n)
+
+def check_response_code(_response, _type):
+    if response_champ.status_code != 200:
+        raise Exception("An Error Occured for:" +_type+ ' Response Code: ' + str(_response.status_code))
 
 #------------------------------------------------------------ FUNCTIONS ------------------------------------------------------------#
 
 
 
 #------------------------------------------------------------ MAIN ------------------------------------------------------------#
-champion = str(sys.argv[1])
+champion = validate_champInput(str(sys.argv[1]))
 #print(champion)
-
-response = requests.get(keys.DDragon+champion+'.json')
-
-#do we have a good request?
-if response.status_code == 200:
-    print('Success!')
-#if we do not get a 200 code, raise an error and stop the program
-else:
-    raise Exception("An Error Occured: "+ champion + " was not found")
+#print ("Number of arguments: " + str(len(sys.argv)))
+items = []
 
 
-championData = response.json()['data']
-championData_stats = response.json()['data'][champion]['stats']
+response_champ = requests.get(ks.DDragon_champs+champion+'.json')
+response_items = requests.get(ks.DDragon_items)
 
-print(championData_stats)
-    
+check_response_code(response_champ, 'CHAMPS')
+check_response_code(response_items, 'ITEMS')
+
+championData = response_champ.json()['data']
+itemData = response_items.json()['data']
+#print(championData[champion]['stats'] )
+
+for i in itemData:
+     if itemData[i]['name'] in sys.argv:
+        #print(itemData[i])
+        items.append(itemData[i])
+
+Item_CleanUp(items)
+
+
+#print(championData[champion]['spells'])
 #create a basic table that shows the champions level 1 stats,  level 18 stats and the per level stat
-output_basic_stats(championData_stats, champion)
+#Basic_Stats_Output(championData[champion]['stats'], champion)
+#Per_Level_Stats_output(championData[champion]['stats'], champion)
+
 #------------------------------------------------------------ MAIN ------------------------------------------------------------#
---
+++ special_scaling_values.py
@@ -0,0 +1,3 @@
+#sometimes champions have special scaling which is not listen in the normal scaling values 
+tristanna_range_per_level = 8
+cass_ms_per_level = 4
